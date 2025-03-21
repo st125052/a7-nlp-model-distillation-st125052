ddbcbd2809eec092c2d986f7033ee35b8b93fb93
Swaraj Bhanja <st125052@ait.ac.th>
2025-03-21 22:47:37 +0700

    Add Docker setup and web app files
    - Added Docker Compose configurations for Flask app.
    - Created a Dockerfile to set up the Python environment.
    - Introduced Nginx configuration for reverse proxying.
    - Updated requirements with necessary libraries.
    - Added HTML structure for the web interface with Bootstrap styling.
    - Implemented JavaScript functionality for form submission and API interaction.
    - Removed an unused Jupyter notebook file.

--- app/static/script.js
+++ app/static/script.js
@@ -1,6 +1,43 @@
-Flask
-Flask-CORS
-Werkzeug
-numpy
-torch
-transformers
+document.getElementById('searchForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Get form input
+    const search = document.getElementById('search').value.trim();
+    let isValid = true;
+
+    // Clear previous error messages
+    document.getElementById('searchTextError').textContent = '';
+
+    // Validate search text input
+    if (!search || search.length === 0) {
+        document.getElementById('searchTextError').textContent = 'Please enter a valid search text.';
+        isValid = false;
+    }
+
+    if (isValid) {
+        predictRelevantContent(search);
+    }
+});
+
+function predictRelevantContent(search) {
+    const apiUrl = `/predict?search=${encodeURIComponent(search)}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            if (Array.isArray(data) && data.length > 0) {
+                const resultContainer = document.getElementById('resultContainer');
+                const searchResultElement = document.getElementById('searchResult');
+
+                searchResultElement.innerHTML = '';
+
+                data.forEach(word => {
+                    searchResultElement.innerHTML += `${word}`;
+                });
+                
+                resultContainer.style.display = 'block';
+            } else {
+                console.error('Unexpected API response format:', data);
+            }
+        }).catch(error => console.error('Error:', error));
+}
